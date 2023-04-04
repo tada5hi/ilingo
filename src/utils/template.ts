8960e7f4393b5f370e380c7fbaf97c76f1927115
tada5hi <peter.placzek1996@gmail.com>
2023-04-04 12:04:48 +0200

    feat: only replace template var if it exists

--- src/utils/template.ts
+++ src/utils/template.ts
@@ -5,12 +5,20 @@
  * view the LICENSE file that was distributed with this source code.
  */
 
-export const template = (
+export function template(
     str: string,
     data: Record<string, any>,
     regex = /\{\{(.+?)\}\}/g,
-) : string => Array.from(str.matchAll(regex))
-    .reduce((
-        acc,
-        match,
-    ) => acc.replace(match[0], data[match[1]]), str);
+) : string {
+    return Array.from(str.matchAll(regex))
+        .reduce((
+            acc,
+            match,
+        ) => {
+            if (typeof data[match[1]] !== 'undefined') {
+                return acc.replace(match[0], data[match[1]]);
+            }
+
+            return acc;
+        }, str);
+}
---
+++ test/unit/utils/template.spec.ts
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2023.
+ * Author Peter Placzek (tada5hi)
+ * For the full copyright and license information,
+ * view the LICENSE file that was distributed with this source code.
+ */
+
+import {template} from "../../../src";
+
+describe('src/utils/template', () => {
+    it('should replace str in template', () => {
+        let str = template('Hi {{name}}!', {name: 'Peter'});
+        expect(str).toEqual('Hi Peter!');
+
+        str = template('Hi {{name}}!', {});
+        expect(str).toEqual('Hi {{name}}!');
+    })
+})
