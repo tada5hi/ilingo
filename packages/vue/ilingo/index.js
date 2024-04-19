4a4e48af5100abcfc533d91ca9b116fa93bf6b68
tada5hi <peter.placzek1996@gmail.com>
2024-04-19 14:55:50 +0200

    feat: enable export paths

--- packages/vue/ilingo/index.js
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2024.
- * Author Peter Placzek (tada5hi)
- * For the full copyright and license information,
- * view the LICENSE file that was distributed with this source code.
- */
-
-// eslint-disable-next-line import/no-extraneous-dependencies
-module.exports = require('ilingo');
